787804cfd1a704faa9a114a088df3382e0b126d9
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-12-11 18:06:50 +0900

    Add datetime handler

---
+++ src/utils/datetime_handler.py
@@ -0,0 +1,18 @@
+import datetime
+import pytz
+
+
+class DatetimeHandler:
+
+    def __init__(self):
+        self.current_datetime = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+
+    def get_current_ime(self):
+        return f'{self.current_datetime:%H:%M:%S}'
+
+    def get_current_date(self):
+        return f'{self.current_datetime:%Y-%m-%d}'
+
+    def get_current_datetime(self):
+        return f'{self.current_datetime:%Y-%m-%d %H:%M:%S}'
+
--- src/utils/file_handler.py
+++ src/utils/file_handler.py
@@ -1,13 +1,28 @@
-import json
+import csv
+
+from utils.datetime_handler import DatetimeHandler
+from configs.configs import FILE_PATH
 
 
 class FileHandler:
 
-    def file_writer(self, title, contents):
-        with open(title, 'w') as f:
+    def write_file(self, title, contents):
+        with open(title + '.txt', 'w') as f:
             for content in contents:
                 f.write(f'{content}\n')
 
-    def file_reader(self, title):
+    def read_file(self, title):
         with open(title, 'r') as f:
             print(f.read())
+
+    def write_csv_file(self, title, contents):
+        datetime = DatetimeHandler()
+        current = datetime.get_current_date()
+        dict_contents = [{'username': content} for content in contents]
+        csv_file_name = FILE_PATH + title + '_' + current + '.csv'
+        with open(csv_file_name, 'w', newline='') as f:
+            field_names = ['username', 'mfa enabled']
+            csv_file = csv.DictWriter(f, fieldnames=field_names)
+            csv_file.writeheader()
+            for row in dict_contents:
+                 csv_file.writerow(row)
