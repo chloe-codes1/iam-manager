30a2601c9ecfae07419bacd0c80826541e613630
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-12-11 18:07:11 +0900

    Modify mfa_handler - add save into csv

--- src/mfa/mfa_handler.py
+++ src/mfa/mfa_handler.py
@@ -12,6 +12,7 @@ class MFAHandler:
         return self.iam_client.list_users()['Users']
 
     def get_users_with_mfa_not_enabled(self):
+        print('Collecting no MFA users started...')
         users = self.list_iam_users()
         no_mfa_users = []
         for user in users:
@@ -19,13 +20,12 @@ class MFAHandler:
             res = self.iam_client.list_mfa_devices(UserName=iam_user)
 
             if not res['MFADevices']:
-                print(f'mfa 없는 당신... {iam_user}')
+                print(f'MFA not enabled user: {iam_user}')
                 no_mfa_users.append(iam_user)
         return no_mfa_users
 
     def save_no_mfa_users_into_file(self):
-        print(f'save_no_mfa_users_into_file called')
         no_mfa_users = self.get_users_with_mfa_not_enabled()
         file_handler = FileHandler()
-        file_handler.file_writer(NO_MFA_USER_FILE, no_mfa_users)
+        file_handler.write_csv_file(NO_MFA_USER_FILE, no_mfa_users)
 
